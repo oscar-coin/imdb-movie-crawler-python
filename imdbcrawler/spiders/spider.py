42313fb7e5585204abdf2830c4cadfca41d0676d
Christian Westhoff <ywesthoffchristian1@gmail.com>
2015-11-06 14:02:08 +0100

    Optimized object memory usage

--- imdbcrawler/spiders/spider.py
+++ imdbcrawler/spiders/spider.py
@@ -25,9 +25,10 @@ class ImdbSpider(scrapy.Spider):
                 continue
 
             item = MovieItem()
-            item['title'] = self.getXpath("td[@class='title']/a/text()", sel, 0)
-            item['imdbId'] = imdbId
-            item['url'] = response.urljoin(baseUrl)
+            title = self.getXpath("td[@class='title']/a/text()", sel, 0)
+            self.setItem(item, 'title', title)
+            self.setItem(item, 'imdbId', imdbId)
+            self.setItem(item, 'url', response.urljoin(baseUrl))
             yield scrapy.Request(response.urljoin(item['url']), meta={'item':item }, callback=self.parseMovies, priority=1)
 
         # get the next page
@@ -40,29 +41,30 @@ class ImdbSpider(scrapy.Spider):
 
         # parse Director
         dir = PersonItem()
-        dir['name'] = self.getXpath("//div[@itemprop='director']/a/span/text()", response, 0)
+        self.setItem(dir, 'name', self.getXpath("//div[@itemprop='director']/a/span/text()", response, 0))
         dirBaseUrl = self.getXpath("//div[@itemprop='director']/a/@href", response, 0)
         if dirBaseUrl:
-            dir['url'] = response.urljoin(dirBaseUrl)
-            dir['imdbId'] = self.resolveId(dirBaseUrl, '/name/')
-        item['director'] = dir
+            self.setItem(dir, 'url', response.urljoin(dirBaseUrl))
+            self.setItem(dir, 'imdbId', self.resolveId(dirBaseUrl, '/name/'))
+        self.setItem(item, 'director', dir)
 
         #parse ranking
-        item['ranking'] = self.getXpath("//*[@id='meterRank']/text()", response, 0)
+        self.setItem(item, 'ranking', self.getXpath("//*[@id='meterRank']/text()", response, 0))
 
         # parse Writers
-        item['writers'] = []
+        writers = []
         for idx, writerSel in enumerate(response.xpath("//div[@itemprop='creator']/a/span/text()").extract()):
             wri = PersonItem()
-            wri['name'] = writerSel.strip()
+            self.setItem(wri, 'name', writerSel.strip())
             wriBaseUrl = self.getXpath("//div[@itemprop='creator']/a["+str(idx+1)+"]/@href", response, 0)
             if wriBaseUrl:
-                wri['url'] = response.urljoin(wriBaseUrl)
-                wri['imdbId'] = self.resolveId(wriBaseUrl, '/name/')
-            item['writers'].append(wri)
+                self.setItem(wri, 'url', response.urljoin(wriBaseUrl))
+                self.setItem(wri, 'imdbId', self.resolveId(wriBaseUrl, '/name/'))
+            writers.append(wri)
+        self.setItem(item, "writers", writers)
 
         # parse genre
-        item['genres'] = self.getXpath("//div[@itemprop='genre']/a/text()", response, -1)
+        self.setItem(item, 'genres', self.getXpath("//div[@itemprop='genre']/a/text()", response, -1))
 
         item = self.parseRating(item, response)
         item = self.getDetails(item, response)
@@ -73,30 +75,31 @@ class ImdbSpider(scrapy.Spider):
     def parseReleaseInfo(self, response):
         # parse the release info
         item = response.meta['item']
-        item["releaseInfo"] = []
+        releaseInfos = []
         for idx, sel in enumerate(response.xpath("//table[@id='release_dates']/tr")):
             if idx >= self.maxReleaseInfo:
                 break
-            current = ReleaseInfoItem()
-            current["Date"] = self.getXpath("td[2]/text()", sel, 0) + " " + self.getXpath("td[2]/a/text()", sel, 0)
-            current["Country"] = self.getXpath("td[1]/a/text()", sel, 0)
-            current["Info"] = self.getXpath("td[3]/text()", sel, 0)
-            item["releaseInfo"].append(current)
+            relInf = ReleaseInfoItem()
+            self.setItem(relInf, "Date", self.getXpath("td[2]/text()", sel, 0) + " " + self.getXpath("td[2]/a/text()", sel, 0))
+            self.setItem(relInf, "Country", self.getXpath("td[1]/a/text()", sel, 0))
+            self.setItem(relInf, "Info", self.getXpath("td[3]/text()", sel, 0))
+            releaseInfos.append(relInf)
+        self.setItem(item, "releaseInfo", releaseInfos)
         return scrapy.Request(item["url"] + "awards?ref_=tt_awd", meta={'item':item }, callback=self.parseAwards, priority=4)
 
     def parseRating(self, item, response):
         # parse imdb rating
-        imdbRating = RatingItem()
-        imdbRating["avgScore"] = self.getXpath("//span[@itemprop='ratingValue']/text()", response, 0)
-        imdbRating["maxScore"] = self.getXpath("//span[@itemprop='bestRating']/text()", response, 0)
-        imdbRating["ratingCount"] = self.getXpath("//span[@itemprop='ratingCount']/text()", response, 0)
-        item["rating"] = imdbRating
+        imdbRat = RatingItem()
+        self.setItem(imdbRat, "avgScore", self.getXpath("//span[@itemprop='ratingValue']/text()", response, 0))
+        self.setItem(imdbRat, "maxScore", self.getXpath("//span[@itemprop='bestRating']/text()", response, 0))
+        self.setItem(imdbRat, "ratingCount", self.getXpath("//span[@itemprop='ratingCount']/text()", response, 0))
+        self.setItem(item, "rating", imdbRat)
         return item
 
     # Parses the award page of a movie
     def parseAwards(self, response):
         item = response.meta['item']
-        item["awards"] = []
+        awards = []
 
         for idx, sel in enumerate(response.xpath("//div[@class='article listo']/table[@class='awards']")):
             name = self.getXpath("//div[@class='article listo']/h3["+str(idx+1)+"]/text()", sel, 0)
@@ -104,34 +107,38 @@ class ImdbSpider(scrapy.Spider):
 
             for idx, sel in enumerate(sel.xpath("tr")):
                 award = AwardItem()
-                award["name"] = name
-                award["year"] = year
-                award["category"] = self.getXpath("td[@class='title_award_outcome']/span[@class='award_category']/text()", sel, 0)
-                award["categoryDesc"] = self.getXpath("td[@class='award_description']/text()", sel, 0)
-                award["outcome"] = self.getXpath("td[@class='title_award_outcome']/b/text()", sel, 0)
-                award["members"] = self.getXpath("td[@class='award_description']/a/text()", sel, -1)
-                item['awards'].append(award)
+                self.setItem(award, "name", name)
+                self.setItem(award, "year", year)
+                self.setItem(award, "category", self.getXpath("td[@class='title_award_outcome']/span[@class='award_category']/text()", sel, 0))
+                self.setItem(award, "categoryDesc", self.getXpath("td[@class='award_description']/text()", sel, 0))
+                self.setItem(award, "outcome", self.getXpath("td[@class='title_award_outcome']/b/text()", sel, 0))
+                self.setItem(award, "members", self.getXpath("td[@class='award_description']/a/text()", sel, -1))
+                awards.append(award)
+
+        self.setItem(item, "awards", awards)
         yield item
 
     # Parses the cast object
     def parseCasts(self, item, response):
-        item['castMembers'] = []
+        castMembers = []
         for idx, sel in enumerate(response.xpath("//*[@id='titleCast']/table/tr")):
             if idx == 0:
                 continue
             cast = CastItem()
-            cast['ranking'] = idx
-            cast['name'] = self.getXpath("td[2]/a/span/text()", sel, 0)
+            self.setItem(cast, 'ranking', idx)
+            self.setItem(cast, 'name', self.getXpath("td[2]/a/span/text()", sel, 0))
             baseUrl = self.getXpath("td[2]/a/@href", sel, 0)
             if baseUrl:
-                cast['url'] = response.urljoin(baseUrl)
-                cast['imdbId'] = self.resolveId(baseUrl, '/name/')
+                self.setItem(cast, 'url', response.urljoin(baseUrl))
+                self.setItem(cast, 'imdbId', self.resolveId(baseUrl, '/name/'))
             # Cast's character name could be a link
             if sel.xpath("td[4]/div/a/text()"):
-                cast['characterName'] = self.getXpath("td[4]/div/a/text()", sel, 0)
+                self.setItem(cast, 'characterName', self.getXpath("td[4]/div/a/text()", sel, 0))
             elif sel.xpath("td[4]/div/text()"):
-                cast['characterName'] = self.getXpath("td[4]/div/text()", sel, 0)
-            item['castMembers'].append(cast)
+                self.setItem(cast, 'characterName', self.getXpath("td[4]/div/text()", sel, 0))
+            castMembers.append(cast)
+
+        self.setItem(item, 'castMembers', castMembers)
         return scrapy.Request(item["url"] + "releaseinfo?ref_=tt_ov_inf", meta={'item':item }, callback=self.parseReleaseInfo, priority=3)
 
     def getDetails(self, item, response):
@@ -144,24 +151,29 @@ class ImdbSpider(scrapy.Spider):
     def mapDetails(self, response, details, item, index):
         if details:
             if "Language:" in details:
-                item['languages'] = self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/a/text()", response, -1)
+                self.setItem(item, 'languages', self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/a/text()", response, -1))
             elif "Country:" in details:
-                item['countries'] = self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/a/text()", response, -1)
+                self.setItem(item, 'countries', self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/a/text()", response, -1))
             elif "Budget:" in details:
-                item['budget'] = self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/text()[2]", response, 0)
+                self.setItem(item, 'budget', re.sub("\s\s+", " "," ".join(self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/text() | //*[@id='titleDetails']/div["+str(index)+"]/*[not(self::h4)]/text()", response, -1)).strip()))
             elif "Gross:" in details:
-                item['grossProfit'] = self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/text()[2]", response, 0)
+                self.setItem(item, 'grossProfit', re.sub("\s\s+", " "," ".join(self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/text() | //*[@id='titleDetails']/div["+str(index)+"]/*[not(self::h4)]/text()", response, -1)).strip()))
             elif "Opening Weekend:" in details:
-                item['openingWeekendProfit'] = re.sub("\s\s+", " ", self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/text()[2]", response, 0))
+                self.setItem(item, 'openingWeekendProfit', re.sub("\s\s+", " ", " ".join(self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/text() | //*[@id='titleDetails']/div["+str(index)+"]/*[not(self::h4)]/text()", response, -1)).strip()))
             elif "Runtime:" in details:
-                item['runtime'] = self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/time/text()", response, 0)
+                self.setItem(item, 'runtime', self.getXpath("//*[@id='titleDetails']/div["+str(index)+"]/time/text()", response, 0))
         return item
+
+    def setItem(self, item, key, property):
+        if not property or (hasattr(property, "__len__") and not len(property)>0):
+            return
+        item[key] = property
 
     def getXpath(self, path, response, index):
         parsed = response.xpath(path)
         if parsed:
             striped = [x.strip() for x in parsed.extract()]
-            if 0 > index > len(striped):
+            if index < 0 or index > len(striped)-1:
                 return striped
             return striped[index]
         return None
